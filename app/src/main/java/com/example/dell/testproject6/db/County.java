e9596bedbc7273d0c7b9a0f3933e5ba3bd8d6f23
lixiujuan <1125493602@qq.com>
2019-12-19 17:17:17 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/dell/testproject6/db/County.java
@@ -0,0 +1,46 @@
+package com.example.dell.testproject6.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Dell on 2019/12/19.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/com/example/dell/testproject6/db/Province.java
@@ -0,0 +1,37 @@
+package com.example.dell.testproject6.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Dell on 2019/12/19.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
